b67b0bc0f55e6bab23b4375a2df91befcae08b9b
Nine7png <163432246+Nine7png@users.noreply.github.com>
2024-04-17 17:53:05 -0300

    Create bee1005.c

---
+++ bee1005.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	
+	float notaA, notaB, media;
+	scanf("%f", &notaA);
+	scanf("%f", &notaB);
+	media = ((notaA*3.5)+(notaB*7.5))/11;
+	printf("MEDIA = %.5f\n", media);
+
+	return 0;
+}

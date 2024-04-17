a4527027b9c677559f48c22d19648ec6ab3f6c1d
Nine7png <163432246+Nine7png@users.noreply.github.com>
2024-04-17 18:13:06 -0300

    Create bee1050.c

---
+++ bee1050.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(){
+
+    int dd;
+    scanf("%d", &dd);
+
+    if(dd == 61){
+        printf("Brasilia\n");
+    }
+    else{
+        if(dd == 71){
+            printf("Salvador\n");
+        }
+        else{
+            if(dd == 11){
+                printf("Sao Paulo\n");
+            }
+            else{
+                if(dd == 21){
+                    printf("Rio de Janeiro\n");
+                }
+                else{
+                    if(dd == 32){
+                        printf("Juiz de Fora\n");
+                    }
+                    else{
+                        if(dd == 19){
+                            printf("Campinas\n");
+                        }
+                        else{
+                            if(dd == 27){
+                                printf("Vitoria\n");
+                            }
+                            else{
+                                if(dd == 31){
+                                    printf("Belo Horizonte\n");
+                                }
+                                else{
+                                    printf("DDD nao cadastrado\n");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
